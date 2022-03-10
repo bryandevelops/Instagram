f914058224f4b87858f95353f686d62a55dc8c35
bryan0147 <bryan0147@gmail.com>
2022-03-10 18:33:38 -0500

    Style the app Login View Controller. Implement Sign in/Sign up functionality.

--- Instagram/Controllers/LoginViewController.swift
+++ Instagram/Controllers/LoginViewController.swift
@@ -6,16 +6,52 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameTextField.text!
+        let password = passwordTextField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { user, error in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                let alert = UIAlertController(title: "Uh Oh!", message: "\(error?.localizedDescription ?? "")", preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameTextField.text
+        user.password = passwordTextField.text
+        
+        user.signUpInBackground { success, error in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                print("success")
+            } else {
+                let alert = UIAlertController(title: "Uh Oh!", message: "\(error?.localizedDescription ?? "")", preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
