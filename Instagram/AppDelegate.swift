2b862396db5a84a00a41b5f651fc313d08d14bc0
bryan0147 <bryan0147@gmail.com>
2022-03-10 15:48:34 -0500

    Connect Parse client to Xcode Project.

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "4rdYSIu9vuSwTX5nKiNBvNAcyO3rRAOB0O7IWXNA" // <- UPDATE
+            $0.clientKey = "eTNvHAAOSokV5u2uhZPtWwGvGe6QXQczPC8ghDzE" // <- UPDATE
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
