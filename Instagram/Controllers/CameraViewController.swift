1959dbeb4f7390a300b16dede4957e9906f782d1
bryan0147 <bryan0147@gmail.com>
2022-03-10 19:43:47 -0500

    Implement Camera and Image Picker in Camera View Controller.

--- Instagram/Controllers/CameraViewController.swift
+++ Instagram/Controllers/CameraViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var cameraImageView: UIImageView!
+    @IBOutlet weak var captionTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-
+    @IBAction func onCameraImageTap(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func onSubmit(_ sender: Any) {
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 375, height: 375)
+        let scaledImage = image.af.imageScaled(to: size)
+        
+        cameraImageView.image = scaledImage
+        dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
